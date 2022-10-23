5ffe85a56ff0e4b45c8dfa35fcd072ea112e6bc8
barteldvn <barteldvn@gmail.com>
2022-10-23 02:07:48 +0200

    - Remove more Lombok

--- src/main/java/me/gleeming/command/CommandHandler.java
+++ src/main/java/me/gleeming/command/CommandHandler.java
@@ -9,30 +9,42 @@ import me.gleeming.command.paramter.ParamProcessor;
 import me.gleeming.command.paramter.Processor;
 import org.bukkit.plugin.Plugin;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 public class CommandHandler {
     private static Plugin plugin;
 
+    public CommandHandler() {
+    }
+
     /**
      * Registers commands based off a file path
      * @param path Path
      */
-    @SneakyThrows
     public static void registerCommands(String path, Plugin plugin) {
-        ClassPath.from(plugin.getClass().getClassLoader()).getAllClasses().stream()
-                .filter(info -> info.getPackageName().startsWith(path))
-                .forEach(info -> registerCommands(info.load(), plugin));
+        try {
+            ClassPath.from(plugin.getClass().getClassLoader()).getAllClasses().stream()
+                    .filter(info -> info.getPackageName().startsWith(path))
+                    .forEach(info -> registerCommands(info.load(), plugin));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * Registers the commands in the class
      * @param commandClass Class
      */
-    @SneakyThrows
     public static void registerCommands(Class<?> commandClass, Plugin plugin) {
         CommandHandler.setPlugin(plugin);
-        registerCommands(commandClass.newInstance());
+        try {
+            registerCommands(commandClass.newInstance());
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -59,25 +71,31 @@ public class CommandHandler {
         });
     }
 
-    @SneakyThrows
     public static void registerProcessors(Processor<?> processorClass, Plugin plugin) {
-        CommandHandler.setPlugin(plugin);
-        ParamProcessor.createProcessor(processorClass);
+        try {
+            CommandHandler.setPlugin(plugin);
+            ParamProcessor.createProcessor(processorClass);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * Registers processors based off a file path
      * @param path Path
      */
-    @SneakyThrows
     public static void registerProcessors(String path, Plugin plugin) {
-        ClassPath.from(plugin.getClass().getClassLoader()).getAllClasses().stream()
-                .filter(info -> info.getPackageName().startsWith(path))
-                .filter(info -> info.load().getSuperclass().equals(Processor.class))
-                .forEach(info -> {
-                    try { ParamProcessor.createProcessor((Processor<?>) info.load().newInstance());
-                    } catch(Exception exception) { exception.printStackTrace(); }
-                });
+        try {
+            ClassPath.from(plugin.getClass().getClassLoader()).getAllClasses().stream()
+                    .filter(info -> info.getPackageName().startsWith(path))
+                    .filter(info -> info.load().getSuperclass().equals(Processor.class))
+                    .forEach(info -> {
+                        try { ParamProcessor.createProcessor((Processor<?>) info.load().newInstance());
+                        } catch(Exception exception) { exception.printStackTrace(); }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static Plugin getPlugin() {
